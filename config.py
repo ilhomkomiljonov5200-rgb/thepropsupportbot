e30ea06bb830303ad424c5d675181f5fbabe4e20
Ilhomm Komiljonov <ilhomkomiljonov3@Ilhomms-MacBook-Air.local>
2026-01-29 03:46:41 +0500

    update thread ids

--- config.py
+++ config.py
@@ -1,9 +1,22 @@
+# ================= BOT =================
 TOKEN = "8361728289:AAEmS7GxROeyyy1r_t0Bo8vLB8UJ6uSt6aY"
 
+
+# ================= GROUP =================
+# asosiy support group
 GROUP_ID = -1003277084936
 
-WITHDRAW_THREAD = 444
-NO_ACCOUNT_THREAD = 443
-TECH_THREAD = 1070
 
+# ================= TOPICS (THREADS) =================
+# oxirgi sondan olindi (topic linkdan)
+
+WITHDRAW_THREAD = 443      # 💸 Pul yechish
+NO_ACCOUNT_THREAD = 2      # ❌ To‘lov qildim akkaunt bermadi
+TECH_THREAD = 444          # ⚠️ Hisob/Dashboard ishlamay
+
+
+# ================= ADMIN =================
 ADMIN_LINK = "https://t.me/thepropsupportuzb"
+
+
+
